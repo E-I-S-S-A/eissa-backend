ed58c398bdb0c9bdb17044f999f9b81c91df45c9
Eissa Faheem <eissafaheem@gmail.com>
2025-01-30 20:07:11 +0530

    feat: get and set keeps done

--- src/main/java/com/eissa/backend/keep/controllers/KeepController.java
+++ src/main/java/com/eissa/backend/keep/controllers/KeepController.java
@@ -14,13 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/keep")
@@ -55,6 +53,22 @@ public class KeepController {
             } else {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/")
+    ResponseEntity<List<Keep>> getKeeps(HttpServletRequest httpServletRequest, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int limit, @RequestParam(required = false) String searchToken) {
+        try {
+            String accessToken = CookieUtil.getCookie(httpServletRequest, CookieEnum.ACCESS_TOKEN);
+
+            if (accessToken.isEmpty() || accessToken == null)
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            String userEmail = JwtUtil.validateToken(accessToken);
+            User userFromDb = userRepo.getUserFromEmailOrId(userEmail);
+            List<Keep> keeps = this.keepRepo.getKeeps(page, limit, searchToken, userFromDb.getUserId());
+            return ResponseEntity.status(HttpStatus.OK).body(keeps);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
--- src/main/java/com/eissa/backend/keep/repos/KeepRepo.java
+++ src/main/java/com/eissa/backend/keep/repos/KeepRepo.java
@@ -2,8 +2,11 @@ package com.eissa.backend.keep.repos;
 
 import com.eissa.backend.keep.models.classes.entities.Keep;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class KeepRepo {
@@ -11,8 +14,16 @@ public class KeepRepo {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public int addKeep(Keep keep){
+    public int addKeep(Keep keep) {
         String query = "insert into keep(keepId, userId, title, description, backgroundColor) values(?,?,?,?,?)";
-        return jdbcTemplate.update(query, keep.getKeepId(), keep.getUserId() ,keep.getTitle(), keep.getDescription(),keep.getBackgroundColor());
+        return jdbcTemplate.update(query, keep.getKeepId(), keep.getUserId(), keep.getTitle(), keep.getDescription(), keep.getBackgroundColor());
+    }
+
+    public List<Keep> getKeeps(int page, int limit, String searchToken, String userId) {
+        String query = "SELECT keepId, title, description, backgroundColor, createdOn " + "FROM keep WHERE userId = ? ORDER BY createdOn DESC LIMIT ? OFFSET ?";
+
+        int offset = (page - 1) * limit;
+
+        return jdbcTemplate.query(query, new BeanPropertyRowMapper<>(Keep.class), userId, limit, offset);
     }
 }
